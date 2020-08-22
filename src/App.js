5888af0cfe8f310dabd210373f1424c16e4bfe10
fady_wasfalla <fady.wasfalla@gmail.com>
2020-08-22 18:33:34 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import displayLesson from './pages/student/displayLesson'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <link
+          rel='stylesheet'
+          href='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+          integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T'
+          crossOrigin='anonymous' />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <Switch>
+        <Route exact path='/display-lesson' component={displayLesson}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/student/displayLesson.js
@@ -0,0 +1,18 @@
+import React  from 'react';
+import {Container, Col} from 'react-bootstrap'
+// import axios from 'axios'
+
+
+function displayLesson() {
+
+  return (
+    <Container fluid >
+    <Col md={12}>
+        
+    <iframe src="https://drive.google.com/file/d/101DdRIuj38xhuGfV8qS7js24L-xa2CYM/preview" title="dance" width="640" height="480"></iframe>
+    </Col>
+    </Container>
+  );
+}
+
+export default displayLesson;
